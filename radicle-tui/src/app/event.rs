omahs/heartwood

tui: Use table model in patch browser

radicle-tui/src/app/event.rs
@@ -1,5 +1,6 @@
-use radicle::cob::patch::{Patch, PatchId};
+use std::str::FromStr;
 
+use radicle_tui::ui::cob::ItemId;
 use tuirealm::command::{Cmd, CmdResult, Direction as MoveDirection};
 use tuirealm::event::{Event, Key, KeyEvent};
 use tuirealm::{MockComponent, NoUserEvent, State, StateValue};
@@ -7,13 +8,12 @@ use tuirealm::{MockComponent, NoUserEvent, State, StateValue};
 use radicle_tui::ui::components::common::container::{GlobalListener, LabeledContainer, Tabs};
 use radicle_tui::ui::components::common::context::{ContextBar, Shortcuts};
 use radicle_tui::ui::components::common::list::PropertyList;
-use radicle_tui::ui::components::common::Browser;
-use radicle_tui::ui::components::home::{Dashboard, IssueBrowser};
+use radicle_tui::ui::components::home::{Dashboard, IssueBrowser, PatchBrowser};
 use radicle_tui::ui::components::patch;
 
 use radicle_tui::ui::widget::Widget;
 
-use super::{HomeMessage, Message, PatchMessage};
+use super::{Message, PatchMessage};
 
 /// Since the framework does not know the type of messages that are being
 /// passed around in the app, the following handlers need to be implemented for
@@ -47,28 +47,30 @@ impl tuirealm::Component<Message, NoUserEvent> for Widget<Tabs> {
     }
 }
 
-impl tuirealm::Component<Message, NoUserEvent> for Widget<Browser<(PatchId, Patch)>> {
+impl tuirealm::Component<Message, NoUserEvent> for Widget<PatchBrowser> {
     fn on(&mut self, event: Event<NoUserEvent>) -> Option<Message> {
         match event {
             Event::Keyboard(KeyEvent { code: Key::Up, .. }) => {
-                match self.perform(Cmd::Move(MoveDirection::Up)) {
-                    CmdResult::Changed(State::One(StateValue::Usize(index))) => {
-                        Some(Message::Home(HomeMessage::PatchChanged(index)))
-                    }
-                    _ => Some(Message::Tick),
-                }
+                self.perform(Cmd::Move(MoveDirection::Up));
+                Some(Message::Tick)
             }
             Event::Keyboard(KeyEvent {
                 code: Key::Down, ..
-            }) => match self.perform(Cmd::Move(MoveDirection::Down)) {
-                CmdResult::Changed(State::One(StateValue::Usize(index))) => {
-                    Some(Message::Home(HomeMessage::PatchChanged(index)))
-                }
-                _ => Some(Message::Tick),
-            },
+            }) => {
+                self.perform(Cmd::Move(MoveDirection::Down));
+                Some(Message::Tick)
+            }
             Event::Keyboard(KeyEvent {
                 code: Key::Enter, ..
-            }) => Some(Message::Patch(PatchMessage::Show)),
+            }) => match self.perform(Cmd::Submit) {
+                CmdResult::Submit(State::One(StateValue::String(id))) => {
+                    match ItemId::from_str(&id) {
+                        Ok(ItemId::Patch(id)) => Some(Message::Patch(PatchMessage::Show(id))),
+                        _ => None,
+                    }
+                }
+                _ => None,
+            },
             _ => None,
         }
     }

radicle-dev/radicle-tui

radicle-tui/src/app/event.rs
@@ -1,21 +1,16 @@
-use radicle::cob::patch::{Patch, PatchId};
-
 use tuirealm::command::{Cmd, CmdResult, Direction as MoveDirection};
 use tuirealm::event::{Event, Key, KeyEvent};
 use tuirealm::{MockComponent, NoUserEvent, State, StateValue};
 
 use radicle_tui::ui::widget::common::container::{GlobalListener, LabeledContainer, Tabs};
 use radicle_tui::ui::widget::common::context::{ContextBar, Shortcuts};
-
 use radicle_tui::ui::widget::common::list::PropertyList;
-
-use radicle_tui::ui::widget::common::Browser;
-use radicle_tui::ui::widget::home::{Dashboard, IssueBrowser};
+use radicle_tui::ui::widget::home::{Dashboard, IssueBrowser, PatchBrowser};
 use radicle_tui::ui::widget::patch;
 
 use radicle_tui::ui::widget::Widget;
 
-use super::{HomeMessage, Message, PatchMessage};
+use super::{Message, PatchMessage};
 
 /// Since the framework does not know the type of messages that are being
 /// passed around in the app, the following handlers need to be implemented for
@@ -50,28 +45,24 @@ impl tuirealm::Component<Message, NoUserEvent> for Widget<Tabs> {
     }
 }
 
-impl tuirealm::Component<Message, NoUserEvent> for Widget<Browser<(PatchId, Patch)>> {
+impl tuirealm::Component<Message, NoUserEvent> for Widget<PatchBrowser> {
     fn on(&mut self, event: Event<NoUserEvent>) -> Option<Message> {
         match event {
             Event::Keyboard(KeyEvent { code: Key::Up, .. }) => {
-                match self.perform(Cmd::Move(MoveDirection::Up)) {
-                    CmdResult::Changed(State::One(StateValue::Usize(index))) => {
-                        Some(Message::Home(HomeMessage::PatchChanged(index)))
-                    }
-                    _ => Some(Message::Tick),
-                }
+                self.perform(Cmd::Move(MoveDirection::Up));
+                Some(Message::Tick)
             }
             Event::Keyboard(KeyEvent {
                 code: Key::Down, ..
-            }) => match self.perform(Cmd::Move(MoveDirection::Down)) {
-                CmdResult::Changed(State::One(StateValue::Usize(index))) => {
-                    Some(Message::Home(HomeMessage::PatchChanged(index)))
-                }
-                _ => Some(Message::Tick),
-            },
+            }) => {
+                self.perform(Cmd::Move(MoveDirection::Down));
+                Some(Message::Tick)
+            }
             Event::Keyboard(KeyEvent {
                 code: Key::Enter, ..
-            }) => Some(Message::Patch(PatchMessage::Show)),
+            }) => self
+                .selected_item()
+                .map(|item| Message::Patch(PatchMessage::Show(item.id()))),
             _ => None,
         }
     }