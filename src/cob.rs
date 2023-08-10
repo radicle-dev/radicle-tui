aa0fe2336c7cd6290bf8b315a1d1489c154a9faa
Erik Kundt <erik@zirkular.io>
2023-08-10 15:53:34 +0200

    Fix labels + assignees parsing on new issue form

--- src/cob.rs
+++ src/cob.rs
@@ -8,10 +8,12 @@ pub mod patch;
 
 pub fn parse_tags(input: String) -> Result<Vec<Tag>> {
     let mut tags = vec![];
-    for name in input.split(',') {
-        match Tag::new(name.trim()) {
-            Ok(tag) => tags.push(tag),
-            Err(err) => return Err(anyhow::anyhow!(err).context("Can't parse tags.")),
+    if !input.is_empty() {
+        for name in input.split(',') {
+            match Tag::new(name.trim()) {
+                Ok(tag) => tags.push(tag),
+                Err(err) => return Err(anyhow::anyhow!(err).context("Can't parse tags.")),
+            }
         }
     }
 
@@ -20,10 +22,12 @@ pub fn parse_tags(input: String) -> Result<Vec<Tag>> {
 
 pub fn parse_assignees(input: String) -> Result<Vec<ActorId>> {
     let mut assignees = vec![];
-    for did in input.split(',') {
-        match ActorId::from_str(did) {
-            Ok(actor) => assignees.push(actor),
-            Err(err) => return Err(anyhow::anyhow!(err).context("Can't parse assignees.")),
+    if !input.is_empty() {
+        for did in input.split(',') {
+            match ActorId::from_str(did) {
+                Ok(actor) => assignees.push(actor),
+                Err(err) => return Err(anyhow::anyhow!(err).context("Can't parse assignees.")),
+            }
         }
     }
 
