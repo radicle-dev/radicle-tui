52b1a2ca877df2007187788b6fb7142f63617c17
Erik Kundt <erik@zirkular.io>
2023-10-16 13:46:36 +0200

    feat: Add converter for issue state

--- src/cob.rs
+++ src/cob.rs
@@ -35,3 +35,31 @@ pub fn parse_assignees(input: String) -> Result<Vec<Did>> {
 
     Ok(assignees)
 }
+
+pub mod state {
+    use anyhow::Result;
+    use radicle::cob::issue::{CloseReason, State};
+
+    pub fn from_u16(input: u16) -> Result<State> {
+        match input {
+            0 => Ok(State::Open),
+            1 => Ok(State::Closed {
+                reason: CloseReason::Solved,
+            }),
+            2 => Ok(State::Closed {
+                reason: CloseReason::Other,
+            }),
+            _ => Err(anyhow::anyhow!("Can't parse state.")),
+        }
+    }
+
+    pub fn to_u16(state: &State) -> u16 {
+        match state {
+            State::Open => 0,
+            State::Closed { reason } => match reason {
+                CloseReason::Solved => 1,
+                CloseReason::Other => 2,
+            },
+        }
+    }
+}
