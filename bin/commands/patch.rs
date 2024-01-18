e8500921cf18b1f85aec2f531dcbcce16fec43a4
Erik Kundt <erik@zirkular.io>
2024-01-18 12:29:37 +0100

    patch: Add --json flag

--- bin/commands/patch.rs
+++ bin/commands/patch.rs
@@ -33,12 +33,14 @@ Select options
 
 Other options
 
+    --json              Output is JSON (default: false)
     --help              Print help
 "#,
 };
 
 pub struct Options {
     op: Operation,
+    json: bool,
 }
 
 pub enum Operation {
@@ -67,6 +69,7 @@ impl Args for Options {
 
         let mut parser = lexopt::Parser::from_args(args);
         let mut op: Option<OperationName> = None;
+        let mut json = false;
         let mut select_opts: Option<SelectOptions> = None;
 
         #[allow(clippy::never_loop)]
@@ -75,15 +78,18 @@ impl Args for Options {
                 Long("help") | Short('h') => {
                     return Err(Error::Help.into());
                 }
+                Long("json") | Short('j') => {
+                    json = true;
+                }
 
                 // Select options.
-                Long("id") if op == Some(OperationName::Select) => {
+                Long("id") | Short('i') if op == Some(OperationName::Select) => {
                     if select_opts.is_some() {
                         anyhow::bail!("select option already given")
                     }
                     select_opts = Some(SelectOptions::new(select::Subject::Id));
                 }
-                Long("operation") if op == Some(OperationName::Select) => {
+                Long("operation") | Short('o') if op == Some(OperationName::Select) => {
                     if select_opts.is_some() {
                         anyhow::bail!("select option already given")
                     }
@@ -103,7 +109,7 @@ impl Args for Options {
                 opts: select_opts.unwrap_or_default(),
             },
         };
-        Ok((Options { op }, vec![]))
+        Ok((Options { op, json }, vec![]))
     }
 }
 
@@ -120,11 +126,17 @@ pub fn run(options: Options, _ctx: impl terminal::Context) -> anyhow::Result<()>
             let mut app = select::App::new(context, opts.subject);
             let output = Window::default().run(&mut app, 1000 / FPS)?;
 
-            let json = output
-                .map(|output| serde_json::to_string(&output).unwrap_or_default())
-                .unwrap_or_default();
+            let output = if options.json {
+                output
+                    .map(|o| serde_json::to_string(&o).unwrap_or_default())
+                    .unwrap_or_default()
+            } else {
+                output
+                    .map(|o| format!("rad patch {}", o))
+                    .unwrap_or_default()
+            };
 
-            eprint!("{json}");
+            eprint!("{output}");
         }
     }
 
