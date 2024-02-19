c2f0df8db45640ea27605e85526bbc865e5472ac
Erik Kundt <erik@zirkular.io>
2024-02-19 17:24:44 +0100

    inbox: Show notifications w/ flux app

--- bin/commands/inbox/select/flux.rs
+++ bin/commands/inbox/select/flux.rs
@@ -1,17 +1,19 @@
 #[path = "flux/ui.rs"]
 mod ui;
 
-use std::default;
-
 use anyhow::Result;
 
-use radicle::node::notifications::Notification;
+use radicle::node::notifications::NotificationId;
+
 use radicle_tui as tui;
-use tui::cob::inbox::Filter;
+
+use tui::cob::inbox::{self, Filter};
 use tui::context::Context;
 use tui::flux::store::{State, Store};
 use tui::flux::termination::{self, Interrupted};
+use tui::flux::ui::cob::NotificationItem;
 use tui::flux::ui::Frontend;
+use tui::Exit;
 
 use crate::tui_inbox::select::flux::ui::ListPage;
 
@@ -24,46 +26,69 @@ pub enum Mode {
 
 pub struct App {
     context: Context,
-    filter: Filter,
+    _filter: Filter,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct InboxState {
-    notifications: Vec<String>,
+    notifications: Vec<NotificationItem>,
+    selected: Option<NotificationId>,
 }
 
-impl InboxState {
-    pub fn notifications(&self) -> &Vec<String> {
-        &self.notifications
+impl TryFrom<&Context> for InboxState {
+    type Error = anyhow::Error;
+
+    fn try_from(context: &Context) -> Result<Self, Self::Error> {
+        let notifications = inbox::all(context.repository(), context.profile())?;
+        let mut items = vec![];
+
+        for notif in &notifications {
+            if let Ok(notif) = NotificationItem::try_from((context.repository(), notif)) {
+                items.push(notif);
+            }
+        }
+
+        Ok(Self {
+            notifications: items,
+            selected: None,
+        })
     }
 }
 
 pub enum Action {
     Exit,
+    Select(NotificationId),
 }
 
 impl State<Action> for InboxState {
+    type Exit = Exit<String>;
+
     fn tick(&self) {}
 
-    fn handle_action(self, action: Action) -> anyhow::Result<bool> {
+    fn handle_action(&mut self, action: Action) -> Option<Exit<String>> {
         match action {
-            Action::Exit => Ok(true),
+            Action::Select(id) => {
+                self.selected = Some(id);
+                None
+            }
+            Action::Exit => Some(Exit { value: None }),
         }
     }
 }
 
 impl App {
     pub fn new(context: Context, filter: Filter) -> Self {
-        Self { context, filter }
+        Self {
+            context,
+            _filter: filter,
+        }
     }
 
     pub async fn run(&self) -> Result<()> {
         let (terminator, mut interrupt_rx) = termination::create_termination();
         let (store, state_rx) = Store::<Action, InboxState>::new();
         let (frontend, action_rx) = Frontend::<Action>::new();
-        let state = InboxState {
-            notifications: vec![],
-        };
+        let state = InboxState::try_from(&self.context)?;
 
         tokio::try_join!(
             store.main_loop(state, terminator, action_rx, interrupt_rx.resubscribe()),
