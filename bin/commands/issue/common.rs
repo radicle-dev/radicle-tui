a719d8f776b70a76e977ef06dd39a1cd27217ded
Erik Kundt <erik@zirkular.io>
2024-02-22 00:19:49 +0100

    issue: Implement select app

--- bin/commands/issue/common.rs
+++ bin/commands/issue/common.rs
@@ -1,3 +1,7 @@
+use std::fmt::Display;
+
+use serde::Serialize;
+
 /// The application's mode. It tells the application
 /// which widgets to render and which output to produce.
 /// Depends on CLI arguments given by the user.
@@ -6,4 +10,33 @@ pub enum Mode {
     #[default]
     Operation,
     Id,
+}
+
+/// The selected issue operation returned by the operation
+/// selection widget.
+#[derive(Clone, Debug, Eq, PartialEq, Serialize)]
+pub enum IssueOperation {
+    Show,
+    Delete,
+    Edit,
+    Comment,
+}
+
+impl Display for IssueOperation {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            IssueOperation::Show => {
+                write!(f, "show")
+            }
+            IssueOperation::Delete => {
+                write!(f, "delete")
+            }
+            IssueOperation::Edit => {
+                write!(f, "edit")
+            }
+            IssueOperation::Comment => {
+                write!(f, "comment")
+            }
+        }
+    }
 }
