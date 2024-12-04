a6d042c3bdc35819d686fcaa504e5c679610a844
Erik Kundt <erik@zirkular.io>
2024-12-04 14:47:09 +0100

    bin: Moves pretty path building

--- bin/ui/span.rs
+++ bin/ui/span.rs
@@ -1,3 +1,6 @@
+use std::path::Path;
+
+use ratatui::prelude::Stylize;
 use ratatui::text::Span;
 
 use crate::cob::HunkState;
@@ -12,4 +15,34 @@ pub fn hunk_state(state: &HunkState) -> Span<'static> {
         // HunkState::Rejected => span::secondary("?"),
         HunkState::Rejected => span::negative("✗"),
     }
+}
+
+pub fn pretty_path(path: &Path, crossed_out: bool, show_path: bool) -> Vec<Span<'static>> {
+    let file = path.file_name().unwrap_or_default();
+    let path = if path.iter().count() > 1 {
+        path.iter()
+            .take(path.iter().count() - 1)
+            .map(|component| component.to_string_lossy().to_string())
+            .collect::<Vec<_>>()
+    } else {
+        vec![]
+    };
+
+    let mut spans = vec![];
+
+    let filename = if crossed_out {
+        span::default(file.to_string_lossy().as_ref()).crossed_out()
+    } else {
+        span::default(file.to_string_lossy().as_ref())
+    };
+    spans.push(filename);
+
+    if show_path {
+        spans.extend([
+            span::default(" "),
+            span::default(&path.join(&String::from("/")).to_string()).dark_gray(),
+        ]);
+    }
+
+    spans
 }
