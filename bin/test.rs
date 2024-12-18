681d95b392ad16772a9075c640a5e37c250ddf1e
Erik Kundt <erik@zirkular.io>
2024-12-18 10:18:38 +0100

    bin: Add more commits to test repo fixtures

--- bin/test.rs
+++ bin/test.rs
@@ -1,13 +1,49 @@
 pub mod fixtures {
+    use std::path::Path;
+
     use anyhow::Result;
 
     use radicle::cob::cache::NoCache;
+    use radicle::git;
     use radicle::patch::{Cache, MergeTarget, PatchMut, Patches};
     use radicle::storage::git::Repository;
+    use radicle::storage::WriteRepository;
     use radicle::test::setup::{BranchWith, NodeWithRepo};
 
+    /// The birth of the radicle project, January 1st, 2018.
+    pub const RADICLE_EPOCH: i64 = 1514817556;
+
     pub fn node_with_repo() -> NodeWithRepo {
-        NodeWithRepo::default()
+        let node = NodeWithRepo::default();
+        let repo = node.repo.repo.raw();
+
+        let sig = git2::Signature::new(
+            "anonymous",
+            "anonymous@radicle.xyz",
+            &git2::Time::new(RADICLE_EPOCH, 0),
+        )
+        .unwrap();
+
+        let head = repo
+            .find_commit(repo.head().unwrap().target().unwrap())
+            .unwrap();
+        let tree =
+            git::write_tree(Path::new("main.rs"), "pub fn main() {}\n".as_bytes(), &repo).unwrap();
+
+        git::commit(
+            &repo,
+            &head,
+            git::refname!("refs/heads/master").as_refstr(),
+            "Another commit",
+            &sig,
+            &tree,
+        )
+        .unwrap();
+
+        drop(tree);
+        drop(head);
+
+        node
     }
 
     pub fn branch_with_eof_removed(node: &NodeWithRepo) -> BranchWith {
